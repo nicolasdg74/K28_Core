522c1385607484371cea480d0f9e5d5c81c69c59
Amore <ndiazgairaud@gmail.com>
2024-01-14 14:15:59 +0100

    añadido necroaracnido

--- Contents/mods/K28_Sounds/media/lua/client/k28Sounds_UI.lua
+++ Contents/mods/K28_Sounds/media/lua/client/k28Sounds_UI.lua
@@ -4,11 +4,10 @@ sonidos_ContextMenu = {};
 
 function sonidos_ContextMenu.reproducir (playerObj, _type)
     -- addSound(playerObj, square:getX(), square:getY(), square:getZ(), radius, volume) <-- addSound es para atraer Zeds, no reproduce sonidos
-    -- playerObj:Say("oh ho " .. _type)
     -- playerObj:playSound(_type) --  <-- con /invisible no funciona
     -- getSoundManager():PlaySound(_type,false,1); -- <-- otra manera de invocar playSound, solo funciona en local
     -- getSoundManager():PlayWorldSound(_type, false, playerObj:getCurrentSquare(), 0.5, 300, 0.7, false) ;
-    getSoundManager():PlayWorldSound(_type, false, playerObj:getCurrentSquare(), 0.5, SandboxVars.K28Sonidos.Distancia, 0.7, false) ;
+    getSoundManager():PlayWorldSound(_type, false, playerObj:getCurrentSquare(), 0.5, 300, 0.7, false) ;
 end
 
 sonidos_ContextMenu.doMenu = function(player, context, worldobjects, test)
@@ -25,22 +24,30 @@ sonidos_ContextMenu.doMenu = function(player, context, worldobjects, test)
     local enteOption = subMenu:addOption("Ente", worldobjects, nil);
     local enteSubMenu = subMenu:getNew(subMenu);
     enteSubMenu:addSubMenu(enteOption, enteSubMenu);    
-    enteSubMenu:addOption("sonidos_Ente_001", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_001");
-    enteSubMenu:addOption("sonidos_Ente_002", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_002");
-    enteSubMenu:addOption("sonidos_Ente_003", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_003");
-    enteSubMenu:addOption("sonidos_Ente_004", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_004");
-    enteSubMenu:addOption("sonidos_Ente_005", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_005");
-    enteSubMenu:addOption("sonidos_Ente_006", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_006");
-    enteSubMenu:addOption("sonidos_Ente_007", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_007");
+    enteSubMenu:addOption("Ente_001", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_001");
+    enteSubMenu:addOption("Ente_002", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_002");
+    enteSubMenu:addOption("Ente_003", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_003");
+    enteSubMenu:addOption("Ente_004", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_004");
+    enteSubMenu:addOption("Ente_005", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_005");
+    enteSubMenu:addOption("Ente_006", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_006");
+    enteSubMenu:addOption("Ente_007", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Ente_007");
 
     -- submenu completo
     local screamerOption = subMenu:addOption("Screamer", worldobjects, nil);
     local screamerSubMenu = subMenu:getNew(subMenu);
     subMenu:addSubMenu(screamerOption, screamerSubMenu);
-    screamerSubMenu:addOption("sonidos_screamer_001", playerObj, sonidos_ContextMenu.reproducir, "sonidos_screamer_001");
-    screamerSubMenu:addOption("sonidos_screamer_002", playerObj, sonidos_ContextMenu.reproducir, "sonidos_screamer_002");
-    screamerSubMenu:addOption("sonidos_screamer_003", playerObj, sonidos_ContextMenu.reproducir, "sonidos_screamer_003");
-    screamerSubMenu:addOption("sonidos_screamer_004", playerObj, sonidos_ContextMenu.reproducir, "sonidos_screamer_004");
+    screamerSubMenu:addOption("Screamer_001", playerObj, sonidos_ContextMenu.reproducir, "sonidos_screamer_001");
+    screamerSubMenu:addOption("Screamer_002", playerObj, sonidos_ContextMenu.reproducir, "sonidos_screamer_002");
+    screamerSubMenu:addOption("Screamer_003", playerObj, sonidos_ContextMenu.reproducir, "sonidos_screamer_003");
+    screamerSubMenu:addOption("Screamer_004", playerObj, sonidos_ContextMenu.reproducir, "sonidos_screamer_004");
+
+    -- submenu completo
+    local NecroaracnidoOption = subMenu:addOption("Necroaracnido", worldobjects, nil);
+    local NecroaracnidoSubMenu = subMenu:getNew(subMenu);
+    subMenu:addSubMenu(NecroaracnidoOption, NecroaracnidoSubMenu);
+    NecroaracnidoSubMenu:addOption("Necroaracnido_001", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Necroaracnido_001");
+    NecroaracnidoSubMenu:addOption("Necroaracnido_002", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Necroaracnido_002");
+    NecroaracnidoSubMenu:addOption("Necroaracnido_003", playerObj, sonidos_ContextMenu.reproducir, "sonidos_Necroaracnido_003");
 
     -- submenu completo
     local ambientalOption = subMenu:addOption("Ambiental", worldobjects, nil);
